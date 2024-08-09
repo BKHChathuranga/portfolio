4a6708ab9937802f8df9b702175a87102c4a4bd7
Hashan Chathuranga <chathurangabkh@gmail.com>
2024-08-09 22:51:15 +0530

    changed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Hashan Chathuranga',
   description: 'Hashan Chathuranga portfolio',
+  keywords:'hashan, chathuranga, Hashan, Chathuranga, HASHAN, CHATHURANGA, HASHAN CHATHURANGA, Hashan Chathuranga, hashan chathuranga'
 }
 
 export default function RootLayout({
